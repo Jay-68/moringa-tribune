76de87932fdfe0061a3053c5470168f265c423b5
Jay-68 <kelvinngari1995@gmail.com>
2019-11-04 11:06:43 +0300

    create a first template

--- news/views.py
+++ news/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime as dt
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def welcome(request):
-    return HttpResponse('Welcome to Moringa Tribune')
+    return render(request, 'welcome.html')
 
 
 def news_of_day(request):
@@ -23,12 +24,35 @@ def news_of_day(request):
       '''
     return HttpResponse(html)
 
+
 def convert_dates(dates):
-  # Function that gets the weekly number of the date.
-  day_number = dt.date.weekday(dates)
+    # Function that gets the weekly number of the date.
+    day_number = dt.date.weekday(dates)
 
-  days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    days = ['Monday', 'Tuesday', 'Wednesday',
+            'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-  # Returning the actual day of the week.
-  day = days[day_number]
-  return day
+    # Returning the actual day of the week.
+    day = days[day_number]
+    return day
+
+
+def past_days_news(request, past_date):
+
+    try:
+        # converts data from the string url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    day = convert_dates(date)
+    html = f'''
+      <html>
+        <body>
+          <h1>News for { day } { date.day }-{ date.month }-{ date.year }</h1>
+        </body>
+      </html>
+      '''
+    return HttpResponse(html)
