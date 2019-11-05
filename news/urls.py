0779c43557eb0615bc2eccd705a1e41e44433eb0
Jay-68 <kelvinngari1995@gmail.com>
2019-11-05 14:50:58 +0300

    article class methods for saving and deleting articles

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.news_of_day, name='NewsToday'),
     path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name='pastNews')
+    path('search/', views.search_results, name='search_results')
 ]
