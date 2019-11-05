ad8c6a230f298aadc5ad492a7c47c94dd9884995
Jay-68 <kelvinngari1995@gmail.com>
2019-11-05 14:47:14 +0300

    add changes:add date for article, update model views

--- news/views.py
+++ news/views.py
@@ -2,18 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 import datetime as dt
 from django.shortcuts import render, redirect
+from .models import Article
 
 # Create your views here.
-
-
-def welcome(request):
-    return render(request, 'welcome.html')
-
 def news_of_day(request):
     date = dt.date.today()
+    news = Article.today_news()
+    return render(request, 'all-news/today-news.html', {'date':date,'news':news})
 
-    # Function to convert date object to find exact day
-    return render(request, 'all-news/today-news.html', {'date':date})
 
 # view function to present news from past days
 def past_days_news(request, past_date):
@@ -26,8 +22,9 @@ def past_days_news(request, past_date):
         # Raise 404 error when ValueError is thrown
         raise Http404()
         assert False
-    
+
     if date == dt.date.today():
       return redirect(news_of_day)
 
-    return render(request, 'all-news/past-news.html', {'date':date})
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {'date':date,'news':news})
